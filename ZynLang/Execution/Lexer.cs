a4f0d2192aa14131ce837488720f6d2b361f43f7
Noah Garrett <67662284+noahgarrett@users.noreply.github.com>
2024-10-04 17:34:55 -0400

    lexer complete with tests

---
+++ ZynLang/Execution/Lexer.cs
@@ -0,0 +1,295 @@
+﻿using ZynLang.Models;
+
+namespace ZynLang.Execution;
+
+public class Lexer
+{
+    public string Source { get; set; } = string.Empty;
+
+    public int Position { get; set; } = -1;
+    public int ReadPosition { get; set; } = 0;
+    public int LineNo { get; set; } = 1;
+
+    public char CurrentChar { get; set; }
+
+    public Lexer(string source)
+    {
+        Source = source;
+        readChar();
+    }
+
+    /// <summary>
+    /// Advances the lexer and returns the next token
+    /// </summary>
+    /// <returns></returns>
+    public Token NextToken()
+    {
+        Token tok;
+
+        skipWhitespace();
+
+        switch (CurrentChar)
+        {
+            case '+':
+                if (peekChar() == '=') // Handle +=
+                {
+                    char ch = CurrentChar;
+                    readChar();
+                    tok = newToken(TokenType.PLUS_EQ, $"{ch}{CurrentChar}");
+                }
+                else if (peekChar() == '+') // Handle ++
+                {
+                    char ch = CurrentChar;
+                    readChar();
+                    tok = newToken(TokenType.PLUS_PLUS, $"{ch}{CurrentChar}");
+                }
+                else // Handle +
+                {
+                    tok = newToken(TokenType.PLUS, CurrentChar);
+                }
+                break;
+            case '-':
+                if (peekChar() == '=') // Handle -=
+                {
+                    char ch = CurrentChar;
+                    readChar();
+                    tok = newToken(TokenType.MINUS_EQ, $"{ch}{CurrentChar}");
+                }
+                else if (peekChar() == '-') // Handle --
+                {
+                    char ch = CurrentChar;
+                    readChar();
+                    tok = newToken(TokenType.MINUS_MINUS, $"{ch}{CurrentChar}");
+                }
+                else // Handle -
+                    tok = newToken(TokenType.MINUS, CurrentChar);
+                break;
+            case '*':
+                if (peekChar() == '=') // Handle *=
+                {
+                    char ch = CurrentChar;
+                    readChar();
+                    tok = newToken(TokenType.MUL_EQ, $"{ch}{CurrentChar}");
+                }
+                else
+                    tok = newToken(TokenType.ASTERISK, CurrentChar);
+                break;
+            case '/':
+                if (peekChar() == '=') // Handle /=
+                {
+                    char ch = CurrentChar;
+                    readChar();
+                    tok = newToken(TokenType.DIV_EQ, $"{ch}{CurrentChar}");
+                }
+                else
+                    tok = newToken(TokenType.SLASH, CurrentChar);
+                break;
+            case '^':
+                tok = newToken(TokenType.POW, CurrentChar);
+                break;
+            case '%':
+                tok = newToken(TokenType.MODULUS, CurrentChar);
+                break;
+            case '<':
+                if (peekChar() == '=') // Handle <=
+                {
+                    char ch = CurrentChar;
+                    readChar();
+                    tok = newToken(TokenType.LT_EQ, $"{ch}{CurrentChar}");
+                }
+                else
+                    tok = newToken(TokenType.LT, CurrentChar);
+                break;
+            case '>':
+                if (peekChar() == '=') // Handle >=
+                {
+                    char ch = CurrentChar;
+                    readChar();
+                    tok = newToken(TokenType.GT_EQ, $"{ch}{CurrentChar}");
+                }
+                else
+                    tok = newToken(TokenType.GT, CurrentChar);
+                break;
+            case '=':
+                if (peekChar() == '=') // Handle ==
+                {
+                    char ch = CurrentChar;
+                    readChar();
+                    tok = newToken(TokenType.EQ_EQ, $"{ch}{CurrentChar}");
+                }
+                else
+                    tok = newToken(TokenType.EQ, CurrentChar);
+                break;
+            case '!':
+                if (peekChar() == '=') // Handle !=
+                {
+                    char ch = CurrentChar;
+                    readChar();
+                    tok = newToken(TokenType.NOT_EQ, $"{ch}{CurrentChar}");
+                }
+                else
+                    tok = newToken(TokenType.BANG, CurrentChar);
+                break;
+            case ':':
+                tok = newToken(TokenType.COLON, CurrentChar);
+                break;
+            case ';':
+                tok = newToken(TokenType.SEMICOLON, CurrentChar);
+                break;
+            case ',':
+                tok = newToken(TokenType.COMMA, CurrentChar);
+                break;
+            case '(':
+                tok = newToken(TokenType.LPAREN, CurrentChar);
+                break;
+            case ')':
+                tok = newToken(TokenType.RPAREN, CurrentChar);
+                break;
+            case '{':
+                tok = newToken(TokenType.LBRACE, CurrentChar);
+                break;
+            case '}':
+                tok = newToken(TokenType.RBRACE, CurrentChar);
+                break;
+            case '"':
+                tok = newToken(TokenType.STRING, CurrentChar);
+                break;
+            case '\0':
+                tok = newToken(TokenType.EOF, CurrentChar);
+                break;
+            default:
+                if (isLetter(CurrentChar))
+                {
+                    string literal = readIdentifier();
+                    TokenType tt = TokenHelper.LookupIdent(literal);
+                    tok = newToken(tt, CurrentChar);
+                    return tok;
+                }
+                else if (isDigit(CurrentChar))
+                {
+                    tok = readNumber();
+                    return tok;
+                }
+                else
+                    tok = newToken(TokenType.ILLEGAL, CurrentChar);
+                break;
+        }
+
+        readChar();
+        return tok;
+    }
+
+    /// <summary>
+    /// Reads the next character from the source code and advances the lexer position
+    /// </summary>
+    private void readChar()
+    {
+        if (ReadPosition >= Source.Length)
+            CurrentChar = '\0';
+        else
+            CurrentChar = Source[ReadPosition];
+
+        Position = ReadPosition;
+        ReadPosition++;
+    }
+
+    /// <summary>
+    /// Peeks to the upcoming character without advancing the lexer position
+    /// </summary>
+    /// <returns></returns>
+    private char? peekChar()
+    {
+        if (ReadPosition >= Source.Length)
+            return null;
+
+        return Source[ReadPosition];
+    }
+
+    /// <summary>
+    /// Skips ignored characters and whitespace
+    /// </summary>
+    private void skipWhitespace()
+    {
+        while (" \t\n\r".Contains(CurrentChar))
+        {
+            if (CurrentChar == '\n')
+                LineNo++;
+
+            readChar();
+        }
+    }
+
+    /// <summary>
+    /// Creates a new token class
+    /// </summary>
+    /// <param name="tt"></param>
+    /// <param name="literal"></param>
+    /// <returns></returns>
+    private Token newToken(TokenType tt, dynamic literal)
+    {
+        return new Token(tt, literal, LineNo, Position);
+    }
+
+    /// <summary>
+    /// Verifies if the character should be treated as a letter
+    /// </summary>
+    /// <param name="ch"></param>
+    /// <returns></returns>
+    private bool isLetter(char ch)
+    {
+        return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_';
+    }
+
+    private bool isDigit(char ch)
+    {
+        return '0' <= ch && ch <= '9';
+    }
+
+    /// <summary>
+    /// Reads an identifier name
+    /// </summary>
+    /// <returns></returns>
+    private string readIdentifier()
+    {
+        int position = Position;
+        while (CurrentChar != '\0' && (isLetter(CurrentChar) || isDigit(CurrentChar)))
+            readChar();
+
+        return Source[position..Position];
+    }
+
+    /// <summary>
+    /// Reads a number from the input file and returns its token
+    /// </summary>
+    /// <returns></returns>
+    private Token readNumber()
+    {
+        int startPos = Position;
+        int dotCount = 0;
+
+        string output = string.Empty;
+        while (isDigit(CurrentChar) || CurrentChar == '.')
+        {
+            if (CurrentChar == '.')
+                dotCount++;
+
+            if (dotCount > 1)
+            {
+                // TODO: fix this you dumb ass :(
+                Console.WriteLine($"Too many decimals in number on line {LineNo}, position {Position}");
+                return newToken(TokenType.ILLEGAL, Source[startPos..Position]);
+            }
+
+            output += Source[Position];
+            readChar();
+
+            if (CurrentChar == '\0')
+                break;
+        }
+
+        if (dotCount == 0)
+            return newToken(TokenType.INT, int.Parse(output));
+        else
+            return newToken(TokenType.FLOAT, float.Parse(output));
+    }
+}
---
+++ ZynLang/Models/Token.cs
@@ -0,0 +1,99 @@
+﻿namespace ZynLang.Models;
+
+public enum TokenType
+{
+    EOF, ILLEGAL,
+
+    // Data Types
+    IDENT, INT, FLOAT, STRING,
+
+    // Arithmetic Types
+    PLUS, MINUS, ASTERISK, SLASH, POW, MODULUS,
+
+    // Prefix Symbols
+    BANG,
+
+    // Postfix Symbols
+    PLUS_PLUS, MINUS_MINUS,
+
+    // Assignment Symbols
+    EQ, PLUS_EQ, MINUS_EQ, MUL_EQ, DIV_EQ,
+
+    // Comparison Symbols
+    LT, LT_EQ, GT, GT_EQ, EQ_EQ, NOT_EQ,
+
+    // Symbols
+    COLON, COMMA, SEMICOLON, LPAREN, RPAREN, LBRACE, RBRACE,
+
+    // Keywords
+    LET, FN, RETURN, IF, ELSE, TRUE, FALSE, WHILE, BREAK, CONTINUE, FOR, IMPORT,
+
+    // Typing
+    TYPE
+}
+
+public class Token(TokenType type, dynamic? literal, int lineNo, int position)
+{
+    public TokenType Type { get; set; } = type;
+    public dynamic? Literal { get; set; } = literal;
+    public int LineNo { get; set; } = lineNo;
+    public int Position { get; set; } = position;
+
+    public string Print()
+    {
+        return $"Token[{Type} : {Literal} : Line {LineNo} : Position {Position}]";
+    }
+}
+
+public static class TokenHelper
+{
+    public static Dictionary<string, TokenType> Keywords = new()
+    {
+        { "let", TokenType.LET },
+        { "fn", TokenType.FN },
+        { "return", TokenType.RETURN },
+        { "if", TokenType.IF },
+        { "else", TokenType.ELSE },
+        { "true", TokenType.TRUE },
+        { "false", TokenType.FALSE },
+        { "while", TokenType.WHILE },
+        { "break", TokenType.BREAK },
+        { "continue", TokenType.CONTINUE },
+        { "for", TokenType.FOR },
+        { "import", TokenType.IMPORT },
+    };
+
+    public static Dictionary<string, TokenType> AltKeywords = new()
+    {
+        { "lit", TokenType.LET },
+        { "bruh", TokenType.FN },
+        { "pause", TokenType.RETURN },
+        { "sus", TokenType.IF },
+        { "imposter", TokenType.ELSE },
+        { "nocap", TokenType.TRUE },
+        { "cap", TokenType.FALSE },
+        { "weee", TokenType.WHILE },
+        { "yeet", TokenType.BREAK },
+        { "anothaone", TokenType.CONTINUE },
+        { "dab", TokenType.FOR },
+        { "gib", TokenType.IMPORT },
+    };
+
+    public static List<string> TypeKeywords = ["int", "float", "bool", "str", "void"];
+
+    public static TokenType LookupIdent(string ident)
+    {
+        bool normalKeywordExists = Keywords.TryGetValue(ident, out TokenType nType);
+        if (normalKeywordExists)
+            return nType;
+
+        bool altKeywordExists = AltKeywords.TryGetValue(ident, out TokenType aType);
+        if (altKeywordExists)
+            return aType;
+
+        if (TypeKeywords.Contains(ident))
+            return TokenType.TYPE;
+
+        return TokenType.IDENT;
+    }
+}
